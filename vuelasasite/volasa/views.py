87156f144daa5637fde1d887a8fd5a85935ffce7
UnopposedQuill <UnopposedQuill@github.com>
2018-11-08 18:44:43 -0600

    Limpiado el código de las vistas; se removió código y comentarios ya no necesitados

--- vuelasasite/volasa/views.py
+++ vuelasasite/volasa/views.py
@@ -1,11 +1,10 @@
+from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
-from  django.http import HttpResponse, Http404
+
 from .models import Vuelo, Cliente
-from django.template import loader
+
 
 # Create your views here.
-# TODO limpiar el código de versiones anteriores en los comentarios
-# TODO limpiar los imports
 # TODO cambiar todas views por view basadas en clases.
 
 def login(request):
@@ -15,12 +14,6 @@ def login(request):
 def cliente(request, cliente_id):
     cliente_request = get_object_or_404(Cliente, pk=cliente_id)
     return render(request, 'volasa/cliente.html', {'cliente': cliente_request})
-    # try:
-    #     cliente = Cliente.objects.get(pk=cliente_id)
-    # except Cliente.DoesNotExist:
-    #     raise Http404
-    # return render(request, 'volasa,cliente.html', {'cliente':cliente})
-    # return HttpResponse("Welcome Cliente")
 
 
 def vuelo(request, vuelo_id):
@@ -29,14 +22,8 @@ def vuelo(request, vuelo_id):
 
 def vuelos(request):
     vuelos_disponibles = Vuelo.objects.order_by('-fechaPartida')[:5]
-    # template = loader.get_template('volasa/vuelos.html')
     context = {'vuelos_disponibles': vuelos_disponibles}
     return render(request, 'volasa/vuelos.html', context)
-
-    # return HttpResponse(template.render(context, request))
-    # Esto era para una vista más básica de pruebas.
-    # output = ', '.join([v.codigoAvion for v in vuelos_disponibles])
-    # return HttpResponse(output)
 
 
 def register(request):
